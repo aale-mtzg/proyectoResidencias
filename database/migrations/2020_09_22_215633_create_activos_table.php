a0e37bcb58038aec2ad10e12f3686dab96369e83
aale-mtzg <66137018+aale-mtzg@users.noreply.github.com>
2020-09-22 16:36:32 -0600

    Tabla activos, migracion y atributos hechos

--- database/migrations/2020_09_22_215633_create_activos_table.php
+++ database/migrations/2020_09_22_215633_create_activos_table.php
@@ -14,8 +14,24 @@ class CreateActivosTable extends Migration
     public function up()
     {
         Schema::create('activos', function (Blueprint $table) {
-            $table->id();
+
+            $table->id('id_activo');
+            $table->string('numero_serial',10);
+            $table->string('numero_serial_dispositivo',10);
+            $table->string('numero_serial_tecNM',10);
+            $table->smallInteger('tipo_activo');
+            $table->string('nombre_activo', 50);
             $table->timestamps();
+            $table->dateTime('fecha_alta');
+            $table->string('marca', 50);
+            $table->string('modelo', 50);
+            $table->string('color', 50);
+            $table->string('descripcion', 50)->nullable();
+            $table->binary('imagen')->nullable();
+            $table->binary('codigo_qr');
+            $table->smallInteger('tipo_ubicacion');
+            $table->string('nombre_ubicacion', 50);
+            $table->smallInteger('tipo_estatus');
         });
     }
 
